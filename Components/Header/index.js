fc213d9e620e8cbcdb1cd51c007d9acba7e23ab5
Isaiah Harris <isaiah.harris.hi@gmail.com>
2019-04-11 19:54:34 -1000

    header and title

--- Components/Header/index.js
+++ Components/Header/index.js
@@ -1,12 +1,31 @@
 import React from 'react';
-import { Text, View } from 'react-native';
+import { Text, View, StyleSheet } from 'react-native';
 
 const Header = () => {
   return (
-    <View className="header">
-      <Text className="title">Header</Text>
+    <View style={styles.header}>
+      <View style={styles.titleContainer}>
+        <Text style={styles.title}>Popper</Text>
+      </View>
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  header: {
+    height: '100%'
+  },
+  titleContainer: {
+    height: '10%',
+    backgroundColor: '#484848',
+    display: 'flex',
+    justifyContent: 'center'
+  },
+  title: {
+    margin: 15,
+    color: '#fa5a53',
+    fontSize: 30
+  }
+});
 
 export default Header;
